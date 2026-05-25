0da8618b3862ac6ecd3a06837b192831ebcfedb1
vanshbaranwal <vanshveerbaranwal@gmail.com>
2026-05-26 02:56:42 +0530

    updated the routes.

--- BACKEND/routes/user.routes.js
+++ BACKEND/routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { login, registerUser, verifyUser } from "../controller/user.controller.js";
+import { getMe, login, logoutUser, registerUser, verifyUser } from "../controller/user.controller.js";
+import { isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.get("/verify/:token", verifyUser);
 router.post("/login", login);
+router.get("/me", isLoggedIn, getMe);
+router.get("/logout", isLoggedIn, logoutUser);
 
 
 export default router;
